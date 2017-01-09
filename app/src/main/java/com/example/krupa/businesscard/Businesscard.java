361886ef4c13d2b3784382fc4e8f87ab1ef97c4c
krupa1 <krupa@Krupamalliks-MacBook-Air.local>
2017-01-09 13:17:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/krupa/businesscard/Businesscard.java
@@ -0,0 +1,13 @@
+package com.example.krupa.businesscard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Businesscard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_businesscard);
+    }
+}
